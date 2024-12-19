55697aa022397e7f77a2d6e0a713c078d1a2fda5
Artem <a.shapovalov@corp.mail.ru>
2024-12-19 10:23:21 +0300

    Speed up Day 19

--- src/main/java/aoc2024/days/Day19.java
+++ src/main/java/aoc2024/days/Day19.java
@@ -3,7 +3,6 @@ package aoc2024.days;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 
 import aoc2024.utils.IO;
@@ -11,7 +10,7 @@ import aoc2024.utils.IO;
 
 public class Day19 extends AbstractDay {
 
-    private HashSet<String> patterns;
+    private List<String> patterns;
     private List<String> designs;
     private HashMap<String, Long> cache;
 
@@ -19,46 +18,33 @@ public class Day19 extends AbstractDay {
     public void prepare(String fn) throws IOException {
         List<String> lines = IO.readLines(fn);
 
-        patterns = new HashSet<>(Arrays.asList(lines.get(0).split(", ")));
+        patterns = Arrays.asList(lines.get(0).split(", "));
+        patterns.sort(null);
         designs = lines.subList(2, lines.size());
-
         cache = new HashMap<>();
+        cache.put("", 1L);
     }
 
-    private boolean isPossible(String design, int start) {
-        if (start == design.length()) return true;
+    private long countPossible(String rem) {
+        if (cache.containsKey(rem)) return cache.get(rem);
 
-        for (String pat: patterns) {
-            if (design.regionMatches(start, pat, 0, pat.length()) && isPossible(design, start + pat.length()))
-                return true;
-        }
-        return false;
-    }
+        long count = patterns.stream()
+            .filter(pat -> rem.startsWith(pat))
+            .map(pat -> countPossible(rem.substring(pat.length())))
+            .reduce(0L, Long::sum);
 
-    private long countPossible(String design, int start) {
-        if (start == design.length()) return 1;
-
-        String rem = design.substring(start);
-        if (cache.containsKey(rem)) {
-            return cache.get(rem);
-        }
-
-        long count = 0;
-        for (String pat: patterns) {
-            if (design.regionMatches(start, pat, 0, pat.length()))
-                count += countPossible(design, start + pat.length());
-        }
         cache.put(rem, count);
+
         return count;
     }
 
     @Override
     public boolean part1Impl(boolean strict) {
-        return check(designs.stream().filter(d -> isPossible(d, 0)).count(), 263L, strict);
+        return check(designs.stream().filter(d -> countPossible(d) > 0).count(), 263L, strict);
     }
 
     @Override
     public boolean part2Impl(boolean strict) {
-        return check(designs.stream().map(d -> countPossible(d, 0)).reduce(0L, Long::sum), 723524534506343L, strict);
+        return check(designs.stream().map(d -> countPossible(d)).reduce(0L, Long::sum), 723524534506343L, strict);
     }
 }
